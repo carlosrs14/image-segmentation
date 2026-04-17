c44276110d010953cc51c7111262e45e163d9894
carlosrs14 <crincones578@gmail.com>
2026-04-17 15:28:26 -0500

    refactor negative img and rice problem

--- src/negative_img.cpp
+++ src/negative_img.cpp
@@ -3,37 +3,45 @@
 
 #define MAX_PIX 255
 
+void negative(cv::Mat& in, cv::Mat& out);
+
 int main(int argc, char **argv) {
     if (argc < 2 ) {
-        perror("Must provide a path");
+        perror("Must provide a path\n");
         return EXIT_FAILURE;
     }
     char* path = argv[1];
 
     cv::Mat img = cv::imread(path);
-    std::vector<cv::Mat> channels;
-
-    split(img, channels);
-
     int rows = img.size().height;
     int cols = img.size().width;
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            channels[0].at<uchar>(i, j) = MAX_PIX - channels[0].at<uchar>(i, j);
-            channels[1].at<uchar>(i, j) = MAX_PIX - channels[1].at<uchar>(i, j);
-            channels[2].at<uchar>(i, j) = MAX_PIX - channels[2].at<uchar>(i, j);
+    cv::Mat out = cv::Mat(rows, cols, CV_8U);
 
-        }
-    }
-    
-    cv::Mat out;
-
-    cv::merge(channels, out);
+    negative(img, out);
 
     cv::imshow("Img Source", img);
     cv::imshow("Img Negative", out);
 
     cv::waitKey(0);
     return EXIT_SUCCESS;
+}
+
+void negative(cv::Mat& in, cv::Mat& out) {
+    std::vector<cv::Mat> channels;
+
+    split(in, channels);
+
+    int rows = in.size().height;
+    int cols = in.size().width;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            channels[0].at<uchar>(i, j) = MAX_PIX - channels[0].at<uchar>(i, j);
+            channels[1].at<uchar>(i, j) = MAX_PIX - channels[1].at<uchar>(i, j);
+            channels[2].at<uchar>(i, j) = MAX_PIX - channels[2].at<uchar>(i, j);
+        }
+    }
+    
+    cv::merge(channels, out);
 }
--- src/rice.cpp
+++ src/rice.cpp
@@ -3,35 +3,50 @@
 
 #define UMBRAL 60
 
+void mask(cv::Mat& in, cv::Mat& out);
+
 int main(int argc, char **argv) {
-    const char *path = "../images/rice.jpg";
+    if (argc < 2) {
+        perror("Must provide a path\n");
+        return EXIT_FAILURE;
+    }
+    char *path = argv[1];
     cv::Mat img = cv::imread(path, cv::IMREAD_COLOR);
 
     std::vector<cv::Mat> channels;
     split(img, channels);
-    cv::Mat processed = channels[0];
+    cv::Mat blue_channel = channels[0];
     
     int rows = img.size().height, cols = img.size().width;
-    cv::imshow("Before", processed);
+    cv::imshow("Before", blue_channel);
     
     cv::Mat out = cv::Mat(rows, cols, CV_8U);
 
-    for (size_t i = 0; i < img.size().height; i++) {
-        int min = 255;
-        for (size_t j = 0; j < img.size().width; j++) {
-            if (min > processed.at<uchar>(i, j)) {
-                min = processed.at<uchar>(i, j);
-            }
-        }
+    mask(blue_channel, out);
 
-        for (size_t j = 0; j < img.size().width; j++) {
-            processed.at<uchar>(i, j) = processed.at<uchar>(i, j) - min;
-            out.at<uchar>(i, j) = processed.at<uchar>(i, j) < UMBRAL? 0: 255;
-        }
-    }
-    cv::imshow("After processed", processed);
+    cv::imshow("After processed", blue_channel);
     cv::imshow("Rice segmented", out);
 
     cv::waitKey(0);
     return EXIT_SUCCESS;
+}
+
+
+void mask(cv::Mat& in, cv::Mat& out) {
+    int rows = in.size().height;
+    int cols = in.size().width;
+
+    for (size_t i = 0; i < rows; i++) {
+        int min = 255;
+        for (size_t j = 0; j < cols; j++) {
+            if (min > in.at<uchar>(i, j)) {
+                min = in.at<uchar>(i, j);
+            }
+        }
+
+        for (size_t j = 0; j < cols; j++) {
+            in.at<uchar>(i, j) = in.at<uchar>(i, j) - min;
+            out.at<uchar>(i, j) = in.at<uchar>(i, j) < UMBRAL? 0: 255;
+        }
+    }
 }
